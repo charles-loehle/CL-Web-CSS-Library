3bebc997727509795cfaed1642e97065bb9ec31f
Charles Loehle <clguitar1@gmail.com>
2021-09-29 09:10:52 -0400

    moved border-box rules to base file

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ const purgecss = require('gulp-purgecss');
 function buildStyles() {
 	// look in the sass folder for any .scss file or subfolder (indicated by **) containing .scss files
 	return src('sass/**/*.scss')
-		.pipe(sass({ outputStyle: 'expanded' }))
+		.pipe(sass({ outputStyle: 'expanded' })) // compile the sass
 		.pipe(purgecss({ content: ['*.html'] }))
-		.pipe(dest('css'));
+		.pipe(dest('css')); // destination file for compiled css
 }
 
 function watchTask() {
-	watch(['sass/**/*.scss', '*.html'], buildStyles);
+	watch(['sass/**/*.scss', '*.html'], buildStyles); // watch all .scss files in the sass folder and watch all .htmo files for changes
 }
 
 exports.default = series(buildStyles, watchTask);
